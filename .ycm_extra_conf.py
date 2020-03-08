e328a7c07b858558433ed0233abee59d53a20d74
Ahn, Seongjin <jam45@naver.com>
2020-03-09 00:41:49 +0900

    add paths to ycm config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,11 +1,22 @@
+
+#TDSEHOME = getenv('TDSEHOME')
+TDSEHOME = __file__
 from os.path import join, exists
-TDSEHOME = "/home/ahn/Dropbox/tdse/numerical/package/tdse"
+#TDSEHOME = "/home/ahn/Dropbox/tdse/numerical/package/tdse"
 assert exists(TDSEHOME)
 TDSEINC = join(TDSEHOME, "include")
-assert exists(TDSEINC)
+#assert exists(TDSEINC)
+
+from os import getenv
+MATRIXHOME = getenv('MATRIXHOME')
+assert exists(MATRIXHOME)
+MATRIX_INC = join(MATRIXHOME, "include")
 
 def Settings( **kwargs ):
   return {
-    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-I', TDSEINC ],
+    'flags': [ 
+        '-x', 'c++', '-Wall', '-Wextra', '-Werror',
+        '-I', TDSEINC, 
+        '-I', MATRIX_INC ],
   }
 
