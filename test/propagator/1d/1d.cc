cb29d0340b9cd5ebd37017314516d05b17a84364
Ahn, Seongjin <jam45@naver.com>
2020-03-12 17:03:09 +0900

    updated 1d propagator example

--- test/propagator/1d/1d.cc
+++ test/propagator/1d/1d.cc
@@ -1,10 +1,15 @@
 #include <complex>
 #include <cstdlib>
 #include <iostream>
+#include <random>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
 
 #include "param.h"
 
 #include "../../../include/propagator/1d.hh"
+#include "../../../include/array.hh"
 
 int main() {
 	
@@ -23,20 +28,68 @@ int main() {
 	const size_t Nx = param.get_int("Nx");
 	const double dx = param.get_double("dx");
 	const double dt = param.get_double("dt");
+	const size_t Nt = param.get_int("Nt");
 
 	double *Vx = new double[Nx];
 	for (double *pVx=Vx, *pVx_max=Vx+Nx; pVx<pVx_max; ++pVx) { *pVx = 0.; }
 
 	Propagator_on_Box_1D prop = Propagator_on_Box_1D(Nx, dx, Vx);
 
-	std::complex<double> wf[Nx];
+	std::complex<double> *wf = new std::complex<double>[Nx];
 
-	prop.propagate(wf, dt, 1);
+	// Assign to random numbers
+//	std::random_device rd;
+//	std::mt19937 gen(rd());
+//	std::uniform_real_distribution<double> unidis(-1., 1.);
+//	for (std::complex<double> *pwf=wf, *pwfmax=wf+Nx; pwf<pwfmax; ++pwf)
+//	{ *pwf = unidis(gen); } // imaginary part is set to zero by default
+
+
+	const size_t Nx_tot = 1+Nx+1;
+	const double L = dx * (Nx_tot-1);
+	for (size_t i=0; i<Nx; ++i) {
+		wf[i] = std::sin(M_PI / L * (i+1)*dx);
+	}	
+
+	// Prepare storage for time-dependent wavefunction
+	std::complex<double> *wf_t_1d = new std::complex<double>[Nx*Nt];
+	std::complex<double> **wf_t = new std::complex<double>*[Nt];
+	for (
+			std::complex<double> **pwf_t=wf_t,**pwf_t_max=wf_t+Nt,*pwf_t_1d=wf_t_1d; 
+			pwf_t<pwf_t_max; 
+			++pwf_t, pwf_t_1d += Nx) 
+	{ *pwf_t = pwf_t_1d; }
 	
+	std::complex<double> *wf_max = wf + Nx;
+	std::copy(wf, wf_max, wf_t[0]);
+
+	for (size_t it=0; it<Nt-1; ++it) {
+		prop.propagate(wf, dt, 1);
+
+		std::cout << "wf[" << it+1 << "]= \n";
+		print_array(wf, Nx);
+
+		std::copy(wf, wf_max, wf_t[it+1]);
+	}
+	
+
+	// Write to output file
+	std::ofstream wf_t_file("wf_t.bin", std::ios::binary);
+	if (!wf_t_file.is_open()) {
+		std::cerr << "[ERROR] Failed to open file for `wf_t`\n";
+		return EXIT_FAILURE;
+	}
+	wf_t_file.write( (char *) wf_t_1d, Nt*Nx*sizeof(std::complex<double>));
+	wf_t_file.close();	
+
 
 	// Release resources
 	prop.~Propagator_on_Box_1D();
 	delete [] Vx;
+	delete [] wf;
+
+	delete [] wf_t;
+	delete [] wf_t_1d;
 
 	// Quit main program
 	return EXIT_SUCCESS;
