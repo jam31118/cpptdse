1ac4827e2dbc0427c9e180e65dda46a841864ff3
Ahn, Seongjin <jam45@naver.com>
2020-03-12 17:00:40 +0900

    add print function for an array

--- include/array.hh
+++ include/array.hh
@@ -2,7 +2,15 @@
 #define _ARRAY_HH_
 
 #include <cstdlib>
+#include <iostream>
 
 int add(double *a, double *b, double *c, size_t N);
+
+template <typename T>
+void print_array(T *a, size_t N) {
+	for (T *pT=a, *pTmax=a+N; pT < pTmax; ++pT) {
+		std::cout << *pT;
+	} std::cout << std::endl;
+}
 
 #endif // _ARRAY_HH_
