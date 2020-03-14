a6df84f94043a50dd04f87d7bdb0a31fc678a00f
Ahn, Seongjin <jam45@naver.com>
2020-03-14 12:52:20 +0900

    clean-up && apply set_2d_view_of_1d routine

--- test/propagator/1d/1d.cc
+++ test/propagator/1d/1d.cc
@@ -13,6 +13,8 @@
 
 int main() {
 	
+	// Extract parameters from file
+	//
 	ParamFile param;
 	try { param = ParamFile("in.param"); }
 	catch (std::exception& e) { 
@@ -24,19 +26,22 @@ int main() {
 		std::cerr << "[ERROR] Failed during default constructor of ParamFile\n"; 
 		return EXIT_FAILURE;
 	}
-
 	const size_t Nx = param.get_int("Nx");
 	const double dx = param.get_double("dx");
 	const double dt = param.get_double("dt");
 	const size_t Nt = param.get_int("Nt");
 
+
+	// Construct a time-independent potential array
 	double *Vx = new double[Nx];
 	set_to_zeros(Vx, Nx);
 
+
+	// Construct a propagator object
 	Propagator_on_Box_1D prop = Propagator_on_Box_1D(Nx, dx, Vx);
 
 	
-	//// Prepare initial state
+	// Prepare initial state
 	//
 	std::complex<double> *wf = new std::complex<double>[Nx];
 	set_to_randoms(wf, Nx); // Initialize wavefunction array to random numbers
@@ -48,28 +53,26 @@ int main() {
 
 
 	// Prepare storage for time-dependent wavefunction
+	// 
 	std::complex<double> *wf_t_1d = new std::complex<double>[Nx*Nt];
 	std::complex<double> **wf_t = new std::complex<double>*[Nt];
-	for (
-			std::complex<double> **pwf_t=wf_t,**pwf_t_max=wf_t+Nt,*pwf_t_1d=wf_t_1d; 
-			pwf_t<pwf_t_max; 
-			++pwf_t, pwf_t_1d += Nx) 
-	{ *pwf_t = pwf_t_1d; }
+	set_2d_view_of_1d(wf_t, wf_t_1d, Nt, Nx);
 	
+
+	// Main Propagation
+	// 
 	std::complex<double> *wf_max = wf + Nx;
 	std::copy(wf, wf_max, wf_t[0]);
-
 	for (size_t it=0; it<Nt-1; ++it) {
 		prop.propagate(wf, dt, 1);
-
 //		std::cout << "wf[" << it+1 << "]= \n";
 //		print_array(wf, Nx);
-
 		std::copy(wf, wf_max, wf_t[it+1]);
 	} std::cout << "[ LOG ] COMPLETE: A propagation with real timestep\n";
 	
 
 	// Write to output file
+	//
 	std::string wf_t_file_name("wf_t.bin");
 	std::ofstream wf_t_file(wf_t_file_name, std::ios::binary);
 	if (!wf_t_file.is_open()) {
@@ -83,12 +86,14 @@ int main() {
 
 
 	// Release resources
+	//
 	prop.~Propagator_on_Box_1D();
 	delete [] Vx;
 	delete [] wf;
 
 	delete [] wf_t;
 	delete [] wf_t_1d;
+
 
 	// Quit main program
 	return EXIT_SUCCESS;
