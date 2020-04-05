26251e9432fd78bb17822e4e02c2044d157c7d29
Ahn, Seongjin <jam45@naver.com>
2020-04-05 16:28:18 +0900

    add normalizer to wf preparation etc

--- test/propagator/1d/wf-t0.py
+++ test/propagator/1d/wf-t0.py
@@ -6,12 +6,15 @@ param = ParameterFile("./in.param")
 Nx, dx = (param[key] for key in ("Nx", "dx"))
 
 #Nx_tot = 1 + Nx + 1
-xarr = 0. + dx * np.arange(Nx)
+xarr = 0. + dx * np.arange(1,Nx+1)
 xmin, xmax = xarr[[0,-1]]
 xmid = 0.2 * (xmin + xmax)
 wf_t0 = exp(-(xarr-xmid)**2).astype(np.complex)
-kx = -3.
+kx = -5.
 wf_t0 *= exp(1.j*kx*xarr)
 wf_t0[[0,-1]] = 0.
+
+from tdse.propagator.box1d import Wavefunction_Uniform_1D_Box
+Wavefunction_Uniform_1D_Box.normalize(wf_t0, dx)
 
 wf_t0.tofile("wf-t0.bin")
