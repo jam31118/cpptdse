4de7cb029530408754fea1a5eeec97adb040cb42
Ahn, Seongjin <jam45@naver.com>
2020-04-12 15:06:35 +0900

    let the test code to save time-dependent wf

--- test/propagator/spharm/spharm.cc
+++ test/propagator/spharm/spharm.cc
@@ -42,6 +42,10 @@ int main() {
 	const size_t Nr = param->get_long("Nr");
 	const double dr = param->get_double("dr");
 	const size_t Nl = param->get_long("Nl");
+	const size_t Nt = param->get_long("Nt");
+	const double dt = param->get_double("dt");
+
+
 
 	double *Vr = new double[Nr];
 	set_to_zeros(Vr, Nr);
@@ -74,6 +78,27 @@ int main() {
 
 
 
+	// Prepare storage for time-dependent wavefunction
+	// 
+	std::complex<double> *wf_t_1d = new std::complex<double>[Nt*wf_length];
+	std::complex<double> **wf_t = new std::complex<double>*[Nt];
+	set_2d_view_of_1d(wf_t, wf_t_1d, Nt, Nr);
+
+
+	//// Propagate wavefunction
+	//
+	double t = 0.;
+	double *tarr = new double[Nt];
+	tarr[0] = t;
+	std::copy(wf, wf_max, wf_t[0]);
+	for (size_t it=0; it<Nt-1; ++it) {
+		prop->propagate(wf, dt, 1);
+		t += dt;
+		tarr[it+1] = t;
+		std::copy(wf, wf_max, wf_t[it+1]);
+	} std::cout << "[ LOG ] COMPLETE: A propagation with real timestep\n";
+
+
 	//// Save data into files
 	//
 	// Save radial coordinate array
@@ -83,12 +108,34 @@ int main() {
 	rarr_file.write((char *) rarr, sizeof(double) * Nr);
 	rarr_file.close();
 	delete [] rarr;
+
 	// Save the initial wavefunction
 	std::ofstream wf_t0_file("wf-t0.bin", std::ios::binary);
 	wf_t0_file.write((char *) wf_t0, sizeof(std::complex<double>) * wf_length);
 	wf_t0_file.close();
 
+	// Save the static potential
+	std::ofstream Vr_file("Vr_real.bin");
+	Vr_file.write((char *) Vr, Nr*sizeof(double));
+	Vr_file.close();
 
+	// Save the time-dependent wavefunction
+	std::string wf_t_file_name("wf_t.bin");
+	std::ofstream wf_t_file(wf_t_file_name, std::ios::binary);
+	wf_t_file.write((char *) wf_t_1d, Nt*wf_length*sizeof(std::complex<double>));
+	wf_t_file.close();	
+	std::cout << "[ LOG ] COMPLETE: Writing wavefunction to a file: " 
+		<< wf_t_file_name << std::endl; 
+
+	// Save the time array
+	std::ofstream tarr_file("tarr.bin", std::ios::binary);
+	tarr_file.write((char *) tarr, sizeof(double) * Nt);
+	tarr_file.close();
+	delete [] tarr;
+
+
+	delete [] wf_t_1d;
+	delete [] wf_t;
 	delete [] wf_t0;
 	delete [] wf;
 	delete [] Vr;
